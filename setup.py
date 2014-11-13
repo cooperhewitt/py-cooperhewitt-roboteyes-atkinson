0fc07dfe3181427a25189bbee6ab67a81ddd3ce2
straup <straup@gmail.com>
2014-11-13 15:12:05 +0100

    setup wrangling

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='cooperhewitt-roboteyes-atkinson',
-      version='0.1',
-      description='',
-      author='Cooper Hewitt Smithsonian Design Museum',
-      url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinso',
-      requires=[
-        # please write me
-        ],
-      packages=[
-        'cooperhewitt',
-        'cooperhewitt.roboteyes',
-        'cooperhewitt.roboteyes.atkinson',
-        ]
-      scripts=[],
-      download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson/releases/tag/v0.1',
-      license='BSD')
+packages = find_packages()
+desc = open("README.md").read(),
+
+setup(
+    name='cooperhewitt.roboteyes.atkinson',
+    namespace_packages=['cooperhewitt'],
+    version='0.2',
+    description='',
+    author='Cooper Hewitt Smithsonian Design Museum',
+    url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson',
+    dependency_links=[
+        'https://github.com/migurski/atkinson/tarball/master#egg=atk-0.1',
+      ],
+    install_requires=[
+        'atk',
+    ],
+    packages=packages,
+    scripts=[],
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson/releases/tag/v0.2',
+    license='BSD')
